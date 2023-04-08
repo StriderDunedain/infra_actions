3a614cf9ddca6b0720ef833144a93b4f7c5c72c4
Максим Трухинов <maks.truxinov@yandex.ru>
2023-04-08 17:12:00 +0300

    Nope, still dumb as a cork

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,11 +1,9 @@
-from http import HTTPStatus
-
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!', status_code=HTTPStatus.OK)
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница', status_code=HTTPStatus.OK)
+    return HttpResponse('А это вторая страница')
